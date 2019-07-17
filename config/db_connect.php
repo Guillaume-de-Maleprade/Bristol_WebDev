596079b580bba895be2dc37a00a584cedac36d3c
klejim <clement.jimenez@outlook.com>
2019-07-17 13:44:25 +0200

    remove accents from firstname before creating the username

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once($_SERVER['DOCUMENT_ROOT'].'/Bristol_WebDev/tools/logger.php');
+
 $db = NULL;
 $json = file_get_contents($_SERVER['DOCUMENT_ROOT']."/Bristol_WebDev/config/db.json");
 $config = json_decode($json, true);
@@ -7,8 +10,9 @@ try {
     $host = $config['host'];
     $dsn = "mysql:host=$host;dbname=$dbname;charset=UTF8";
     $db = new PDO($dsn, $config["username"], $config["password"]);
-
+    myLog("connection to database successful");
     //echo "Connection success!";
 } catch (PDOException $e) {
+    myLog("connection to database failed");
     die("Connection failed " . $e->getMessage());
 }
--- staff/staff_add.php
+++ staff/staff_add.php
@@ -1,10 +1,10 @@
 <?php
 
-require($_SERVER['DOCUMENT_ROOT'].'/Bristol_WebDev/tools/logger.php');
-require($_SERVER['DOCUMENT_ROOT'].'/Bristol_WebDev/Class/Staff.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/Bristol_WebDev/tools/logger.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/Bristol_WebDev/Class/Staff.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/Bristol_WebDev/tools/utils.php');
 
 print_r($_POST);
-myLog("M'enfin?");
 
 if(isset($_POST['name']) && isset($_POST['firstname'])){
     myLog("adding a staff member");
@@ -12,7 +12,7 @@ if(isset($_POST['name']) && isset($_POST['firstname'])){
     $name = htmlspecialchars($_POST['name']);
     $firstname = htmlspecialchars($_POST['firstname']);
 
-    $username = str_replace(" ", "-", $name ).".". $firstname;
+    $username = str_replace(" ", "-", $name ).".". normalizeChars($firstname);
     $username = strtolower($username);
     $i = "";
     while(Staff::readByUserName($username . $i)!= NULL){
