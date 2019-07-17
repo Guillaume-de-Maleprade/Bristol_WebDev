313b9b08d4d5c1595d0ad650cf124844d02b20b0
leacrepin <lea.crepin@hotmail.fr>
2019-07-17 16:11:58 +0100

    user

--- Class/User.php
+++ Class/User.php
@@ -4,36 +4,56 @@ require ($_SERVER['DOCUMENT_ROOT'].'/Bristol_WebDev/config/db_connect.php');
 
 class User
 {
+    public $id;
     public $username;
     public $mail;
     public $name;
     public $firstname;
     public $address;
     public $role;
+    public $password;
 
 
-    public function __construct($mail, $name, $firstname, $address, $username, $role)
+    public function __construct($mail, $name, $firstname, $address, $username, $role, $password = null, $id = null)
     {
+        $this->id = $id;
         $this->username = $username;
         $this->mail = $mail;
         $this->name = $name;
         $this->firstname = $firstname;
         $this->address = $address;
         $this->role = $role;
+        $this->password = $password;
     }
 
     // CREATE
     public function insert()
     {
         try {
+            $password = newPassword();
             $db = $GLOBALS['db'];
-            $query = "INSERT INTO user (username, mail, name, firstname, address, role) VALUES (?,?,?,?,?,?)";
+            $query = "INSERT INTO user (username, mail, name, firstname, address, role, password) VALUES (?,?,?,?,?,?,?)";
             $stmt = $db->prepare($query);
-            $stmt->execute([$this->username, $this->mail, $this->name, $this->firstname, $this->address, $this->role]);
+            $stmt->execute([$this->username, $this->mail, $this->name, $this->firstname, $this->address, $this->role, $this->password]);
+            file_put_contents($_SERVER['DOCUMENT_ROOT'].'/Bristol_WebDev/logs/password.txt', $username.' - '.$password . PHP_EOL, FILE_APPEND | LOCK_EX);
         } catch (PDOException $e) {
             die($e);
         }
     }
+
+    //CREATE PASSWORD (length 8)
+    public function newPassword()
+    {
+        $password=substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(8/strlen($x)) )),1,8);
+        $this->password = password_hash($password,1);
+        return $password;
+    }
+
+    // GETTER
+    public function getRole(){
+        return getRoleFromIndex($this->role);
+    }
+
 
     // READ
     public static function readByUserName($username)
@@ -47,7 +67,7 @@ class User
         }
         $stArray = [];
         foreach ($array as $object) {
-            $user = new User($object['mail'], $object['name'], $object['firstname'], $object['address'], $object['username'], $object['role']);
+            $user = new User($object['mail'], $object['name'], $object['firstname'], $object['address'], $object['username'], $object['role'],  $object['password'],  $object['id']);
             array_push($stArray, $user);
         }
         return $stArray;
@@ -64,7 +84,7 @@ class User
         }
         $stArray = [];
         foreach ($array as $object) {
-            $user = new User($object['mail'], $object['name'], $object['firstname'], $object['address'], $object['username'], $role);
+            $user = new User($object['mail'], $object['name'], $object['firstname'], $object['address'], $object['username'], $object['role'],  $object['password'],  $object['id']);
             array_push($stArray, $user);
         }
         return $stArray;
@@ -115,7 +135,7 @@ class User
         }
         $stArray = [];
         foreach ($array as $object) {
-            $user = new User($object['mail'], $object['name'], $object['firstname'], $object['address'], $object['username']);
+            $user = new User($object['mail'], $object['name'], $object['firstname'], $object['address'], $object['username'], $object['role'],  $object['password'],  $object['id']);
             array_push($stArray, $user);
         }
         return $stArray;
